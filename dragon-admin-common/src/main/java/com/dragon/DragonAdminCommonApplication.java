8d626fa670a0072c0763a8cdcfee91b59793d7ba
ymzyf2007 <yumin_860619@126.com>
2021-05-18 01:29:53 +0800

    [*] 初始化提交

---
+++ dragon-admin-common/src/main/java/com/dragon/DragonAdminCommonApplication.java
@@ -0,0 +1,13 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonAdminCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonAdminCommonApplication.class, args);
+    }
+
+}
---
+++ dragon-admin-common/src/test/java/com/dragon/DragonAdminCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonAdminCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
